3300d5e92c6a210ded2173af79e39fd56aef507c
Efimov <izolyato@yandex.ru>
2022-11-25 01:03:21 +0300

    008Magic

---
+++ 008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Magik
+Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("*");
+int xa = 13;
+int ya = 1;
+int xb = 1;
+int yb = 10;
+int xc = 25;
+int yc = 10;
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("*");
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("*");
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("*");
