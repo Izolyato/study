8c45bf8b238431c1d667ea2822f08ec8116d4859
Efimov <izolyato@yandex.ru>
2022-11-25 00:17:04 +0300

    006if/else

---
+++ 006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Если ты Cаша = тебе большой привет иначе пока (if/else)
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "саша")
+{
+Console.WriteLine("Приветствую вас Александр");
+}
+else
+{
+    Console.Write ("Пока  ");
+    Console.WriteLine (username);
+}
