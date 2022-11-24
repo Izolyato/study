edef331a229f42049182f2c8383c95894b2679ab
Efimov <izolyato@yandex.ru>
2022-11-24 23:20:30 +0300

    004delenie

---
+++ 004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Деление двух целых чисел
+// первый вариант, будет выводить только целые числа
+//int namber1 = 25;
+//int namber2 = 5;
+//Console.WriteLine(namber1/namber2);
+// Второй вариант будет выводить вещественные числа (0,1 0,23 и тд)
+double namber1 = 24;
+double namber2 = 5;
+Console.WriteLine(namber1/namber2);
