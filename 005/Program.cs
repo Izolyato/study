9be25323796852c76ec66ce0052e476d74f329ac
Efimov <izolyato@yandex.ru>
2022-11-24 23:38:28 +0300

    005random

---
+++ 005/Program.cs
@@ -0,0 +1,7 @@
+﻿// Случайности
+
+int namber1 = new Random().Next(1,10);
+Console.WriteLine(namber1);
+int namber2 = new Random().Next(1,10);
+Console.WriteLine(namber2);
+Console.WriteLine(namber1 + namber2);
