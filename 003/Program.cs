6ffe8db058ee9a47995b9aaa3275e06dda78d404
Efimov <izolyato@yandex.ru>
2022-11-24 23:04:54 +0300

    003sum

---
+++ 003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Сложение двух целых чисел
+// Первый вариант
+//int namber1 = 22;
+//int namber2 = 26;
+//int result = namber1 + namber2;
+//Console.WriteLine(result);
+// Второй вариант
+int namber1 = 25;
+int namber2 = 35;
+Console.WriteLine(namber1+namber2);
