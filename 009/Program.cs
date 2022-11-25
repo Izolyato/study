8e8dd9bbb847bb766b20f425ddcb4606633d1b03
Efimov <izolyato@yandex.ru>
2022-11-25 23:40:03 +0300

    009ToInt32

---
+++ 009/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число и получите его квадрат");
+int num = Convert.ToInt32 (Console.ReadLine());
+ int kvadrat = num * num;
+Console.WriteLine($"Квадрат числа  {num} = {kvadrat}");
+
+
+
