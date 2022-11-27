64bec3cca858782d6d476bfa5a1101c326319d60
Efimov <izolyato@yandex.ru>
2022-11-28 00:02:22 +0300

    012otNdo-N

---
+++ 012/Program.cs
@@ -0,0 +1,16 @@
+﻿// Все числа от N до -N
+Console.WriteLine("Введите чило");
+int n = Convert.ToInt32 (Console.ReadLine());
+int nminus = 0;
+if (n < 0)
+{
+    n = -n;
+    nminus = -n;
+}
+else nminus = -n;
+
+while (nminus <= n)
+{
+    Console.Write($"{nminus} ");
+    nminus++;
+}
---
+++ 013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимаем на входе трёхзначное число на выходе получаем последнюю цифру числа.
+Console.WriteLine("Введите тёхзначное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int lastnum = 0;
+if  ( number < 0 )
+{
+    number = number * -1;
+}
+if (number < 999 && number > 99 )
+{
+    lastnum = number%10;
+    Console.WriteLine($"Последняя цифра {lastnum}");
+}
+else Console.WriteLine("Число не трёхзначное");
