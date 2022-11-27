feaa7a3b52949c9929716219ebd09b71595753db
Efimov <izolyato@yandex.ru>
2022-11-28 00:07:17 +0300

    009

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,8 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// 1) Программа принимает на ввод число
+// 2) Выдаёт его квадрат (число умноженное само на себя)
 Console.WriteLine("Введите число и получите его квадрат");
 int num = Convert.ToInt32 (Console.ReadLine());
- int kvadrat = num * num;
-Console.WriteLine($"Квадрат числа  {num} = {kvadrat}");
+int kvadrat = num * num;
+Console.WriteLine($"Квадрат числа {num} = {kvadrat}");
 
 
 
