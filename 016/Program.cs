8ea3e10af164271a00e653f10add7cac6fa16c68
Efimov <izolyato@yandex.ru>
2022-12-02 23:16:04 +0300

    016

---
+++ 016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа которая принимает на вход одно число и проверяет является ли оно чётным
+// (делится ли оно на два без остатка)
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("чётное");
+}
+else
+{
+    Console.WriteLine("не чётное");
+}
