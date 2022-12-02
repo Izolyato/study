ed987b6d8d7db09d8e5c5bd97f2cab1b748b9277
Efimov <izolyato@yandex.ru>
2022-12-03 00:23:20 +0300

    017

---
+++ 017/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа которая принимает на вход число N и выводит все чётные числа от 1 до N
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
