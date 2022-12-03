077550e2a2b9fb5efe270dc66622d24700c7cd7e
Efimov <izolyato@yandex.ru>
2022-12-03 22:50:57 +0300

    019

---
+++ 019/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принимает на входе девять чисел и выдводит максимальное
+Console.WriteLine("Введите девять чисел");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int number4 = Convert.ToInt32(Console.ReadLine());
+int number5 = Convert.ToInt32(Console.ReadLine());
+int number6 = Convert.ToInt32(Console.ReadLine());
+int number7 = Convert.ToInt32(Console.ReadLine());
+int number8 = Convert.ToInt32(Console.ReadLine());
+int number9 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max < number2) max = number2;
+if (max < number3) max = number3;
+if (max < number4) max = number4;
+if (max < number5) max = number5;
+if (max < number6) max = number6;
+if (max < number7) max = number7;
+if (max < number8) max = number8;
+if (max < number9) max = number9;
+Console.WriteLine($"Максимальное число --> {max}");
+
